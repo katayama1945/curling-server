05d339722bf69aeb2a5319a7fb49964972d7ce3c
暉 片山 <katayama@noesis.co.jp>
2025-05-04 09:06:29 +0900

    ネット対戦 申し込み

--- server.js
+++ server.js
@@ -83,6 +83,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('🚀 サーバー起動 http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`🚀 サーバー起動 http://localhost:${port}`);
 });
